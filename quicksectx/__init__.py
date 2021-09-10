ac368ff8cb653bf7ae67727958a73e7ae0a448d0
Anthony Ebiner <45410382+anthonyebiner@users.noreply.github.com>
2021-09-10 06:49:37 +0000

    quick fix

--- quicksectx/__init__.py
+++ quicksectx/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import unicode_literals
-from .quicksectx import Interval, IntervalNode, IntervalTree, distancex
 from .version import __version__
+import pyximport
+pyximport.install()
+from .quicksectx import Interval, IntervalNode, IntervalTree, distancex
